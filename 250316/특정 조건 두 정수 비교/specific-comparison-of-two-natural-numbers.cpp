cafe917d33ae285bf3cc047d3c184a4eaf771567
dev-everyday <everyharus@gmail.com>
2025-03-16 16:42:43 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250316/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    if(A<B){
+        cout << "1 " ;
+    } else {
+        cout << "0 " ;
+    }
+    if(A==B){
+        cout << "1" << endl; 
+    } else {
+        cout << "0" << endl;
+    }
+    return 0;
+}
