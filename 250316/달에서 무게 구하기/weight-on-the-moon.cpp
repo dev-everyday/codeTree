e99c20660f4064c4a6094581504fddc491407288
dev-everyday <everyharus@gmail.com>
2025-03-16 16:16:24 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250316/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int weight = 13;
+    double moon = 0.165;
+
+    printf("%d * %.6f = %.6f", weight, moon, weight * moon);
+
+    return 0;
+}
