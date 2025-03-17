a0424675d065af975a517c1a42133ce7ec637945
dev-everyday <everyharus@gmail.com>
2025-03-17 15:20:11 +0900

    Title: 차례로 출력, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

--- 250317/차례로 출력/print-in-order.java
+++ 250317/차례로 출력/print-in-order.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=5;i<18;i++){
+            System.out.printf("%d ", i);
+        }
     }
 }
