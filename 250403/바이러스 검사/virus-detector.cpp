eb985114499e08bbf2a68cad188f6762e50f5a39
dev-everyday <everyharus@gmail.com>
2025-04-03 22:34:35 +0900

    Title: 바이러스 검사, Time: 365ms, Memory: 4MB, Status: Wrong Answer - Codetree

---
+++ 250403/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+    }
+    int leaderCnt=0, memberCnt=0;
+    cin >> leaderCnt >> memberCnt;
+    int cnt = 0;
+    for(int i=0;i<n;i++){
+        int num = v[i];
+        if(num-leaderCnt<0){
+            cnt++;
+        } else {
+            num-=leaderCnt;
+            cnt++;
+            if(num%memberCnt!=0){
+                cnt++;
+            }
+            cnt+=num/memberCnt;
+        }
+    }  
+    cout << cnt <<endl;
+    return 0;
+}
